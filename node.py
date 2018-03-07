a24b29b04c45755e92cbd04fb333c147e737daad
William Harvey <harveyw25@mycu.concord.edu>
2018-03-07 12:33:28 -0500

    MonteCarlo Tree Completed
    Theoretical Expansion, Selection, Simulation, and Backpropagation
    implemented
    
    Bug in Selection if all child nodes have been visited on selected node

--- node.py
+++ node.py
@@ -1,15 +1,21 @@
 import math
 
 class node:
-    parent = 0
-    timesVisited = 0
-    value = 0
-    children = []
-    selectionConstant = 2
+
+    def __init__(self):
+        self.name = ""
+        self.parent = None
+        self.timesVisited = 0
+        self.value = 0
+        self.children = []
+        self.selectionConstant = 2
+        self.root = False
 
     def selectionScore(self, n):
         if self.timesVisited == 0:
             return math.inf
+        elif n == 999999999:
+            return -99999999999999999
         else:
             return ((self.value/self.timesVisited)+self.selectionConstant*(math.sqrt(math.log(n)/self.timesVisited)))
 
--- tree.py
+++ tree.py
@@ -1,56 +1,93 @@
 from node import *
 import random
+import time
 
 class tree:
     root = node()
+    root.root = True
+    root.timesVisited = 1
+    root.name += "1"
 
+
+    def printNode(self, node):
+        print("Name:", node.name, "Visited:", node.timesVisited, "Value:", node.value, "Children", len(node.children))
+    
     def selection(self):
         highestScore = node()
-        highestScore.timesVisited = math.inf
+        highestScore.timesVisited = 999999999
         temp = self.root
 
+        i = 0
+        
         while temp.timesVisited != 0:
+            
             for x in temp.children:
+                highestScore = node()
+                highestScore.timesVisited = 999999999
+                print("comparing", x.name, highestScore.name)
                 if x.selectionScore(self.root.timesVisited) > highestScore.selectionScore(self.root.timesVisited):
                     highestScore = x
-            temp = x
-            
+                    #print(x.name, "Wins")
+            temp = highestScore
+            i += 1
+            if temp.timesVisited == 999999999 or i == 1000:
+                return 0
+
         return temp
 
     def expansion(self, Node):
-        actions = random.randint(1, 5)
-
-        for x in range(0, actions+1):
+        actions = random.randint(0, 4)
+        for x in range(0, actions):
             nodeToAdd = node()
-            nodeToAdd.parent = node
-            
+            nodeToAdd.parent = Node
+            nodeToAdd.name = nodeToAdd.parent.name + "-" + str(x)
             Node.children.append(nodeToAdd)
 
     def simulation(self, Node):
-        Node.value = random.randint(-20, 20)
+        Node.value = random.randint(0, 20)
+
 
     def backprop(self, n):
-        valueToAdd = n.value
-        times = n.timesVisited
-        n.timesVisited = times + 1 
-        n = node.parent
-        
-        while(n != self.root):
-            times = n.timesVisited
-            n.timesVisited = times + 1
-            n.value = valueToAdd + node.value
+        temp = n
 
-    def print(self, node):
-        print(node.timesVisited, node.value)
+        valueToAdd = n.value
+        
+        times = temp.timesVisited
+        temp.timesVisited = times + 1
+        temp = temp.parent
+        
+        while temp.root == False:
+            times = temp.timesVisited
+            temp.timesVisited = times + 1
+            temp.value = valueToAdd + temp.value
+            temp = temp.parent
+
+        times = temp.timesVisited
+        temp.timesVisited = times + 1
+        temp.value = valueToAdd + temp.value
+        
+
+    def cont(self):
+        node = self.selection()
+        if node != 0:
+            self.simulation(node)
+            self.expansion(node)
+            self.backprop(node)
+            self.printNode(node)
+            return False
+        else:
+            return True
+    
         
 MC = tree()
-
 MC.expansion(MC.root)
-Node = MC.selection()
-MC.print(Node)
-MC.simulation(Node)
-MC.print(Node)
-MC.backprop(Node)
 
-MC.print(MC.root)
+timer = time.time() + 5
+
+end = False
+
+MC.printNode(MC.root)
+
+while (not end) or (time.time() < timer):
+    end = MC.cont()    
 
